c5584ab7bd3a942dd7f7472fb9e3bf531dd5f30c
nyusuyo <hz.ir.daff@gmail.com>
2018-08-07 16:44:06 +0700

    Iseng

---
+++ bash/sayang.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+
+echo "sayangku: iya sayang "
+read lagi apa
+echo "mengetik..."
+read 
+echo "sayangku: lagiketakutan nih"
+read takut kenapa
+echo "mengetik..."
+read
+echo "sayangku: takut kehilangan kamu"
+read 
+echo "mengetik..."
+read
+echo "sayangku: :D :D ,sayangku lagi apa? "
+read lagi nyari seauatu nih  
+echo "mengetik..."
+read
+echo "sayangku: nyariapa sayang ?"
+read nyari cara buat mutusin kamu
+echo "mengetik..."
+read
+echo "sayangku: :'( :'( kamu jahat"
+read biarin aja abis gw cape pacaran mulu dosanya gede
+echo "mengetik..."
+read
+echo "sayangku: :'( "
+read aku gk mau kita bahagia dalam dosa
+echo "mengetik..."
+read
+echo "sayangku: :'("
+read aku mau halalin kamu biar kita bisa bahagia dalam ikatan yg halal
+echo "mengetik..."
+read
+echo "sayangku: :D :D :D ya ampun...sayang..."
+echo "sayangku: terus kapan kamu mau halalin aku ?"
+read sabar ya sayang aku lagi usaha dulu
+echo "mengetik..."
+read
+echo "sayangku: emang kamu usaha apa sayang ?"
+read usaha buat deketin ayah kamu
+echo "mengetik..."
+read
+echo "sayangku: ahahaha...,hati2 lho sayang ayah aku galak lho"
+read gpp sayang lelaki sejati itu akan melakukan apapun demiorang yang dia cintai
+echo "mengetik..."
+read
+echo "sayangku: aduhh...sayang..."
+echo "sayangku: makin sayang deh sama kamu :* :D"
+read iya sayang aku jugaudah dulu ya sayang aku mau usaha dulu
+echo "mengetik..."
+read
+echo "sayangku: iya sayang,yudah gih sana "
+read sayang nanti kamu mau mahar apa
+echo "mengetik..."
+read
+echo "sayangku: apa aja sayang yg penting gk ngeberatin hati sayang"
+read oke deh sayang tunggu aku ya sayang
+echo "mengetik..."
+read
+echo "sayangku: oke sayang :*"
+read bye 
+echo "mengetik..."
+read
+echo "bye too"
